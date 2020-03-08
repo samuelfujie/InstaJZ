22f56a79492e9edcb3c8798f5d5115bcff66b8b7
samuelfujie <samuel.fujie@gmail.com>
2020-03-07 18:43:06 -0800

    Create Post model + sync & migrate database + update admin + Master/Detail interface for read

--- Insta/views.py
+++ Insta/views.py
@@ -1,4 +1,15 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+
+from Insta.models import Post
 
 class HelloWorld(TemplateView):
     template_name = 'test.html'
+
+class PostsView(ListView):
+    model = Post
+    template_name = 'index.html'
+    context_object_name = 'posts'   # change for name access in 'index.html'
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
